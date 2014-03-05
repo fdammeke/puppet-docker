10d65b1ff09772d825bc5b2b8d955c4ef89dfbaa
fdammeke <fabian.dammekens@cegeka.be>
2014-03-05 11:43:24 +0100

    Initial docker install

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,5 +12,15 @@
 #
 class docker {
 
+  package { 'docker-io':
+    ensure => installed
+  }
+
+  service { 'docker':
+    ensure      => running,
+    enable      => true,
+    hasrestart  => true,
+    require     => Package['docker-io']
+  }
 
 }
